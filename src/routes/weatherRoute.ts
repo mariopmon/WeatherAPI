909a29de5868d5e327e1913eb2161fa482842e8a
mariopmon <161736021+mariopmon@users.noreply.github.com>
2024-05-23 19:47:43 +0100

    VS code to GitHub

---
+++ src/routes/weatherRoute.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { getWeatherData } from "../controllers/weatherController.js";
+import { validateCityName } from "../middleware/validators.js";
+
+// We will create a router object
+const router = express.Router();
+
+// We will create a route for the weather data based on the city name
+router.get("/:city", validateCityName, getWeatherData);
+
+router.get("/:london", validateCityName, getWeatherData);
+
+// We will export the router
+export default router;
